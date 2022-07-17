20c13b1062678cb983b657e6af3cd92f77f18d29
Anirudh Sriram <anirudh.sriram96@gmail.com>
2022-07-17 18:25:37 -0400

    Basic UI with working JS implementation.

--- script.js
+++ script.js
@@ -12,34 +12,59 @@ function computerPlay() {
     return(move);
 }
 
-function round(playerSelection, computerSelection) {
+function round(e) {
     let result;
 
-    if(playerSelection === "invalid choice") {
-        result = "You entered an invalid move!";
-    } else if(playerSelection === computerSelection) {
-        result = "It's a tie!";
+    let computerSelection = computerPlay();
+    let playerSelection = e.target.textContent.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    const breakElement = document.createElement('br');
+
+    if(playerSelection === computerSelection) {
+        result = "This round is a tie!";
     } else if(  (playerSelection === "rock" && computerSelection === "scissors") ||
                 (playerSelection === "paper" && computerSelection === "rock") ||
                 (playerSelection === "scissors" && computerSelection === "paper")) {
-        result = "Player wins!";
+        playerScore++;
+        result = "Player wins this round!";
     } else {
-        result = "Computer wins!";
+        computerScore++;
+        result = "Computer wins this round!";
     }
-    return(result);
+
+    const resultElement = document.querySelector('.round-result');
+    const gameResultElement = document.querySelector('.game-result');
+    const roundElement = document.querySelector('.round');
+    const playerScoreElement = document.querySelector('.player-score');
+    const computerScoreElement = document.querySelector('.computer-score');
+
+    resultElement.textContent = `You played ${playerSelection} and the computer played ${computerSelection}. ` + result + ' ';
+    resultElement.appendChild(breakElement);
+    roundElement.textContent = `Round ${++roundCount}`;
+    playerScoreElement.textContent = `${playerScore}`
+    computerScoreElement.textContent = `${computerScore}`
+
+    if((roundCount = roundCount % 5) == 0) {
+        gameResultElement.textContent += 'Game over... ';
+
+        if(playerScore > computerScore)
+            gameResultElement.textContent += 'player wins!';
+        else if(playerScore < computerScore)
+            gameResultElement.textContent += 'computer wins!';
+        else   
+            gameResultElement.textContent += 'it\'s a tie!';
+        playerScore = 0;
+        computerScore = 0;
+    } else
+    gameResultElement.textContent = '';
 }
 
-function game() {
-    //let playerSelection = prompt("Please enter your choice", "");
-    let computerSelection = computerPlay();
-    
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+//let playerSelection = prompt("Please enter your choice", "");
+const playerButtons = [...document.querySelectorAll('.player-button')];
+let roundCount = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-    if(!(playerSelection === "rock") ||
-        (playerSelection === "paper") ||
-        (playerSelection === "scissors"))
-        playerSelection = "invalid choice";
-    console.log("And the result is...\n");
-    console.log(round(playerSelection, computerSelection));
-}
+playerButtons.forEach(button => {
+    button.addEventListener('click', round)
+});
